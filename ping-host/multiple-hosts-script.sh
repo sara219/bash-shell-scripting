e700f4860bc71b9efe32052ebe9077424b55094b
sara219 <sara97abuawwad@gmail.com>
2022-11-03 12:33:07 +0200

    ping host with diff cases

--- ping-host/multiple-hosts-script.sh
+++ ping-host/multiple-hosts-script.sh
@@ -3,7 +3,7 @@
 # Date: 03/11/2022
 # Description: This script will ping multiple remote host and notify.
 
-hosts="./hosts-ips"
+hosts="./hosts-ips-lists"
 
 for ip in $(cat $hosts); do
     ping -c1 $ip &>/dev/null
